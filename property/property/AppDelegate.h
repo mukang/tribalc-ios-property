b3dc38b488a6e0fc802c127470daafd9c1bf4982
wangshuaifeng <wangshuaifeng@buluo-gs.com>
2016-12-27 16:11:43 +0800

    [新增]创建项目

---
+++ property/property/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  property
+//
+//  Created by 王帅锋 on 16/12/27.
+//  Copyright © 2016年 杭州部落公社科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ property/property/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  property
+//
+//  Created by 王帅锋 on 16/12/27.
+//  Copyright © 2016年 杭州部落公社科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
