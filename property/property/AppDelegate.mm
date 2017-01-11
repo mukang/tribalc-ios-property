96ab46989bbeb8d198a93102e5d09801055159db
穆康 <mukang@buluo-gs.com>
2017-01-11 11:55:34 +0800

    [新增] 新增启动页

--- property/property/AppDelegate.mm
+++ property/property/AppDelegate.mm
@@ -29,6 +29,7 @@
     
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    application.statusBarHidden = NO;
     
     TCSipAPI *sipApi = [TCSipAPI api];
     
